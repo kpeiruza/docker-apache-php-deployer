e28ecdc51bf7204fe606f873854c31ea3ea99d0c
Kenneth Peiruza <kenneth@floss.cat>
2018-03-03 09:54:36 +0100

    1st functional version

---
+++ bootstrap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DOCROOT="/var/www/html"
+APACHEUSER="www-data"
+
+if [ -z "$SRCURI" ]
+then
+	echo "Bad usage, please provide the SRCURI environment variable"
+	exit 64
+fi
+
+cd $DOCROOT
+rm -fr *
+
+#	Download file
+wget -q --no-check-certificate $SRCURI
+filename=$(ls)
+
+#	Uncompress file
+if [ $(file $filename | grep -c "Zip archive data") -eq 1 ]
+then
+	unzip $filename
+else
+	tar xf $filename
+fi
+
+rm $filename
+
+#	Move subfolders if necessary
+if [ $(ls | wc -l) -eq 1 ]
+then
+	subfolder=$(ls)
+	mv $subfolder/* $subfolder/.* . 2>/dev/null
+	rmdir $subfolder
+fi
+chown -R $APACHEUSER: .
+
+/usr/sbin/apachectl -D FOREGROUND
